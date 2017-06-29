52ebd0d6096993855319ed1885b914e7d9719fa5
hachi-naki <kent.0810.pc@gmail.com>
2017-06-29 16:13:11 +0900

    yes!

--- src/main.cxx
+++ src/main.cxx
@@ -1,5 +1,43 @@
+#include <string>
 #include <iostream>
-
+#include <vector>
+#include <time.h>
 int main() {
-	std::cout << "Hello world!" << std::endl;
+  int coin = 10;  //持ち時間
+  time_t start = time(NULL);
+  while(1){
+    std::string str;
+    std::getline(std::cin,str);
+    // std::cin.ignore();
+    std::string find_word(" ");
+    std::string::size_type pos = str.find(find_word);
+    std::size_t n = str.size();
+
+    //お金を追加
+    if(str == "10yen"){
+      coin +=15;
+    } if(str == "100yen"){
+      coin += 120;
+    }if(str == "1000yen"){
+      coin += 1000;
+    }
+    time_t now = time(NULL);
+    time_t t = now - start;
+    int l = coin -t;
+    std::cout<< l <<std::endl;
+    if(0 < l && l <= 5){
+      std::cout << "お金を入れてください" << std::endl;
+    }
+    if(l <= 0){
+      std::cout << "GOODBYE" << std::endl;
+    }
+
+    if(str == "connect" || str=="exit"|| t == coin){
+      break;
+    } 
+    //  if(pos == std::string::npos){
+    // std::cout << "not space fond" << std::endl;
+    // }
+    std::cout << str.substr(0,pos) << std::endl;
+  }
 }
