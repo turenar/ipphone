88a665ca0f41e568b7c8c1fb68a84064689eff04
Soma Ueda <sora@turenar.xyz>
2017-07-01 16:30:24 +0900

    :green_heart: fix test

--- test/ipp/util/buffer_test.cxx
+++ test/ipp/util/buffer_test.cxx
@@ -7,59 +7,61 @@
 namespace ipp {
 	namespace util {
 		namespace {
-			template <typename ElementType, typename Array>
-			void test_write(std::size_t expected, buffer<ElementType>& buf, const Array& w,
-			                std::size_t begin, std::size_t len) {
-				ASSERT_EQ(expected, buf.write(w + begin, len));
-			}
+			namespace buffer_test {
+				template <typename ElementType, typename Array>
+				void test_write(std::size_t expected, buffer<ElementType>& buf, const Array& w,
+				                std::size_t begin, std::size_t len) {
+					ASSERT_EQ(expected, buf.write(w + begin, len));
+				}
 
-			template <typename ElementType, typename Array>
-			void test_read(std::size_t expected, buffer<ElementType>& buf, const Array& w, Array& r,
-			               std::size_t begin, std::size_t len) {
-				ASSERT_EQ(expected, buf.read(r, len));
-				std::basic_string<ElementType> wstr(w + begin, expected);
-				std::basic_string<ElementType> rstr(r, expected);
-				ASSERT_EQ(wstr, rstr);
-			}
+				template <typename ElementType, typename Array>
+				void test_read(std::size_t expected, buffer<ElementType>& buf, const Array& w, Array& r,
+				               std::size_t begin, std::size_t len) {
+					ASSERT_EQ(expected, buf.read(r, len));
+					std::basic_string<ElementType> wstr(w + begin, expected);
+					std::basic_string<ElementType> rstr(r, expected);
+					ASSERT_EQ(wstr, rstr);
+				}
 
-			template <typename ElementType>
-			void test_generic() {
+				template <typename ElementType>
+				void test_generic() {
 #define TEST_WRAP(expr) do { SCOPED_TRACE(#expr); expr; if (::testing::Test::HasFatalFailure()) { return; }} while(false)
-				buffer<ElementType> buf(4);
-				const ElementType w[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-				ElementType r[std::extent<decltype(w)>::value] = {};
-				ASSERT_TRUE(buf.empty());
-				ASSERT_FALSE(buf.full());
-				TEST_WRAP(test_write(1, buf, w, 0, 1));
-				ASSERT_FALSE(buf.empty());
-				TEST_WRAP(test_write(2, buf, w, 1, 2));
-				ASSERT_FALSE(buf.full());
-				TEST_WRAP(test_write(1, buf, w, 3, 3));
-				ASSERT_TRUE(buf.full());
-				TEST_WRAP(test_write(0, buf, w, 6, 4));
-				ASSERT_TRUE(buf.full());
-				TEST_WRAP(test_read(1, buf, w, r, 0, 1));
-				ASSERT_FALSE(buf.full());
-				TEST_WRAP(test_read(2, buf, w, r, 1, 2));
-				TEST_WRAP(test_read(1, buf, w, r, 3, 3));
-				ASSERT_TRUE(buf.empty());
-				// this should block...
-				// TEST_WRAP(test_read(1, buf, w, r, 0, 1));
-				TEST_WRAP(test_write(4, buf, w, 0, 4));
-				TEST_WRAP(test_read(3, buf, w, r, 0, 3));
-				TEST_WRAP(test_write(3, buf, w, 4, 4));
-				TEST_WRAP(test_read(4, buf, w, r, 3, 4));
+					buffer<ElementType> buf(4);
+					const ElementType w[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+					ElementType r[std::extent<decltype(w)>::value] = {};
+					ASSERT_TRUE(buf.empty());
+					ASSERT_FALSE(buf.full());
+					TEST_WRAP(test_write(1, buf, w, 0, 1));
+					ASSERT_FALSE(buf.empty());
+					TEST_WRAP(test_write(2, buf, w, 1, 2));
+					ASSERT_FALSE(buf.full());
+					TEST_WRAP(test_write(1, buf, w, 3, 3));
+					ASSERT_TRUE(buf.full());
+					TEST_WRAP(test_write(0, buf, w, 6, 4));
+					ASSERT_TRUE(buf.full());
+					TEST_WRAP(test_read(1, buf, w, r, 0, 1));
+					ASSERT_FALSE(buf.full());
+					TEST_WRAP(test_read(2, buf, w, r, 1, 2));
+					TEST_WRAP(test_read(1, buf, w, r, 3, 3));
+					ASSERT_TRUE(buf.empty());
+					// this should block...
+					// TEST_WRAP(test_read(1, buf, w, r, 0, 1));
+					TEST_WRAP(test_write(4, buf, w, 0, 4));
+					TEST_WRAP(test_read(3, buf, w, r, 0, 3));
+					TEST_WRAP(test_write(3, buf, w, 4, 4));
+					TEST_WRAP(test_read(4, buf, w, r, 3, 4));
+				}
 			}
 		}
 
 		TEST(BufferTest, SingleByte) {
 			SCOPED_TRACE("test_generic<char>");
-			test_generic<char>();
+			buffer_test::test_generic<char>();
 		}
 
 		TEST(BufferTest, MultiByte) {
 			SCOPED_TRACE("test_generic<short>");
-			test_generic<char16_t>();
+			buffer_test::test_generic<char16_t>();
 		}
 	}
 }
--- test/ipp/util/buffer_test_mt.cxx
+++ test/ipp/util/buffer_test_mt.cxx
@@ -7,67 +7,75 @@
 namespace ipp {
 	namespace util {
 		namespace {
-			constexpr int loop_count = 100000;
+			namespace buffer_test_mt {
+				constexpr int loop_count = 100000;
 
-			template <typename ElementType>
-			void test_generic() {
-				auto worker = g3::LogWorker::createLogWorker();
-				worker->addSink(std::make_unique<ipp::logger::log_console_sink>(), &ipp::logger::log_console_sink::log);
-				g3::initializeLogging(worker.get());
+				template <typename ElementType>
+				void test_generic() {
+					auto worker = g3::LogWorker::createLogWorker();
+					worker->addSink(std::make_unique<ipp::logger::log_console_sink>(),
+					                &ipp::logger::log_console_sink::log);
+					g3::initializeLogging(worker.get());
 
-				buffer<ElementType> buf(4);
-				std::mutex m;
-				std::condition_variable cv;
-				std::atomic<bool> ready(false);
-				std::atomic<int> read_count(0);
-				std::atomic<int> write_count(0);
+					buffer<ElementType> buf(4);
+					std::mutex m;
+					std::condition_variable cv;
+					std::atomic<bool> ready(false);
+					std::atomic<int> read_count(0);
+					std::atomic<int> write_count(0);
 
-				std::thread a([&m, &cv, &ready, &buf, &write_count] {
+					std::thread a([&m, &cv, &ready, &buf, &write_count] {
+						{
+							std::unique_lock<std::mutex> lock(m);
+							cv.wait(lock, [&ready]() -> bool { return ready; });
+						}
+						LOG(DEBUG) << "wake up writer";
+						int wc = 0;
+						const ElementType dat[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+						for (int i = 0; i < loop_count; i++) {
+							wc += buf.write(dat, std::extent<decltype(dat)>::value);
+						}
+						write_count = wc;
+					});
+					std::thread b([&m, &cv, &ready, &buf, &read_count, &a] {
+						{
+							std::unique_lock<std::mutex> lock(m);
+							cv.wait(lock, [&ready]() -> bool { return ready; });
+						}
+						LOG(DEBUG) << "wake up reader";
+						int rc = 0;
+
+						ElementType dat[10];
+						for (int i = 0; i < loop_count; i++) {
+							rc += buf.read(dat, std::extent<decltype(dat)>::value);
+						}
+						if (a.joinable()) {
+							a.join();
+						}
+						read_count = rc;
+					});
 					{
-						std::unique_lock<std::mutex> lock(m);
-						cv.wait(lock, [&ready]() -> bool { return ready; });
+						std::lock_guard<std::mutex> lock(m);
+						ready = true;
+						cv.notify_all();
 					}
-					LOG(DEBUG) << "wake up writer";
-					int wc = 0;
-					const ElementType dat[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-					for (int i = 0; i < loop_count; i++) {
-						wc += buf.write(dat, std::extent<decltype(dat)>::value);
+					if (a.joinable()) {
+						a.join();
 					}
-					write_count = wc;
-				});
-				std::thread b([&m, &cv, &ready, &buf, &read_count] {
-					{
-						std::unique_lock<std::mutex> lock(m);
-						cv.wait(lock, [&ready]() -> bool { return ready; });
-					}
-					LOG(DEBUG) << "wake up reader";
-					int rc = 0;
-
-					ElementType dat[10];
-					for (int i = 0; i < loop_count; i++) {
-						rc += buf.read(dat, std::extent<decltype(dat)>::value);
-					}
-					read_count = rc;
-				});
-				{
-					std::lock_guard<std::mutex> lock(m);
-					ready = true;
-					cv.notify_all();
+					b.join();
+					ASSERT_EQ(write_count, read_count);
 				}
-				a.join();
-				b.join();
-				ASSERT_EQ(write_count, read_count);
 			}
 		}
 
 		TEST(BufferTestMT, SingleByte) {
 			SCOPED_TRACE("test_generic<char>");
-			test_generic<char>();
+			buffer_test_mt::test_generic<char>();
 		}
 
 		TEST(BufferTestMT, MultiByte) {
 			SCOPED_TRACE("test_generic<short>");
-			test_generic<char16_t>();
+			buffer_test_mt::test_generic<char16_t>();
 		}
 	}
 }
