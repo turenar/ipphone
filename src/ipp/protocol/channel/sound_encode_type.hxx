cf8037bb4375f4a73ddceaa3c56af6a0492ae238
Soma Ueda <sora@turenar.xyz>
2017-08-01 18:18:08 +0900

    fix comment

--- src/ipp/protocol/channel/sound_encode_type.hxx
+++ src/ipp/protocol/channel/sound_encode_type.hxx
@@ -6,7 +6,7 @@ namespace ipp {
 	namespace protocol {
 		namespace channel {
 			enum class sound_encode_type : std::uint8_t {
-				raw16, // 16bit/sample 1channel
+				raw16, // 16bit/sample 2channel
 			};
 		}
 	}
